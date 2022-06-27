e972789074a2bb3200cf0a2ad840dc7bd356f52e
Diego Bravo <dbravo_2015@hotmail.com>
2022-06-27 14:13:34 -0300

    Establish model's relations

--- controllers/task.controllers.js
+++ controllers/task.controllers.js
@@ -1,4 +1,5 @@
 const {Tasks}=require('../models/tasks.models')
+const {User}=require("../models/user.models");
 const { catchAsync } = require("../utils/catchAsync.utils");
 const { appError } = require('../utils/appError.utils');
 
@@ -18,7 +19,7 @@ const createTask = catchAsync(async (req, res, next) => {
 }); 
 
 const getAllTask= catchAsync(async(req, res, next)=>{
-    const tasks = await Tasks.findAll();
+    const tasks = await Tasks.findAll({include:User});
     res.status(200).json({
       status: "success",
       tasks,
--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const { app } = require('./app');
 // Utils
 const { db } = require('./utils/database.utils');
 
+const { User } = require("./models/user.models");
+const { Tasks } = require("./models/tasks.models");
+
 db.authenticate()
 	.then(() => console.log('Db authenticated'))
 	.catch(err => console.log(err));
+
+	  // Establish model's relations
+User.hasMany(Tasks, { foreignKey: "userId" });
+Tasks.belongsTo(User);
 
 db.sync()
 	.then(() => console.log('Db synced'))
